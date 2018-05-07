3f372c3f764c84f80b30a7bcc9a55fd99e15d364
Sebastian Mair <sebastian.mair@tngtech.com>
2018-05-07 14:51:22 +0200

    Moved some parameters of Visualizer.visualize to the constructor of Visualizer. Improved the VisualExtension: now one can create a visualization of the analyzed classes including the violations of all checked rules in this class that belong to the same classes-object (which classes and therefore which violations should be exported is defined by a method that has to be called after the test methods). This is possible when using ArchUnitRunner and when using another test-runner. Created test for VisualExtension and added an example-usage of it to two test-classes in package exampletest.

--- archunit-example/example-plain/src/test/java/com/tngtech/archunit/exampletest/CodingRulesTest.java
+++ archunit-example/example-plain/src/test/java/com/tngtech/archunit/exampletest/CodingRulesTest.java
@@ -4,19 +4,18 @@ import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.example.ClassViolatingCodingRules;
 import com.tngtech.archunit.lang.CompositeArchRule;
+import com.tngtech.archunit.visual.VisualExtension;
+import org.junit.AfterClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
-import static com.tngtech.archunit.library.GeneralCodingRules.ACCESS_STANDARD_STREAMS;
-import static com.tngtech.archunit.library.GeneralCodingRules.NO_CLASSES_SHOULD_ACCESS_STANDARD_STREAMS;
-import static com.tngtech.archunit.library.GeneralCodingRules.NO_CLASSES_SHOULD_THROW_GENERIC_EXCEPTIONS;
-import static com.tngtech.archunit.library.GeneralCodingRules.NO_CLASSES_SHOULD_USE_JAVA_UTIL_LOGGING;
+import static com.tngtech.archunit.library.GeneralCodingRules.*;
 
 @Category(Example.class)
 public class CodingRulesTest {
 
-    private final JavaClasses classes = new ClassFileImporter().importPackagesOf(ClassViolatingCodingRules.class);
+    private static final JavaClasses classes = new ClassFileImporter().importPackagesOf(ClassViolatingCodingRules.class);
 
     @Test
     public void classes_should_not_access_standard_streams_defined_by_hand() {
@@ -44,4 +43,9 @@ public class CodingRulesTest {
                 .and(NO_CLASSES_SHOULD_THROW_GENERIC_EXCEPTIONS).check(classes);
     }
 
+
+    @AfterClass
+    public static void createVisualization() {
+        VisualExtension.createVisualization(classes);
+    }
 }
